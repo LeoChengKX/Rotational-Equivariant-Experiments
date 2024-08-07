b243bd37d315bcfa7929a1fa9d0d30af548cb3af
LEO KAIXUAN CHENG <ckx1939@hotmail.com>
2024-08-07 11:14:07 -0400

    Modifications

--- benchmark/MLP_gconv.py
+++ benchmark/MLP_gconv.py
@@ -13,7 +13,7 @@ def load_mlp_weights(ckpt_path):
     weights = []
     cp_trans = []
     for key in x_model.keys():
-        if key[:-2] == 'cparams_transform':
+        if key[:7] == 'cparams':
             cp_trans.append(x_model[key])
         elif key != 'model.mtx1' and key != 'model.mtx2':
             weights.append(x_model[key])
@@ -79,4 +79,72 @@ class MLPGConv_Z2(nn.Module):
         out = torch.stack(out)
 
         return out.transpose(0, 1).reshape(-1, self.ch_out, 4, self.cnn_out_dim, self.cnn_out_dim)
+
+
+class MLPGConv_P4(nn.Module):
+    def __init__(self, ckpt_path, img_dim, ch_in, ch_out, ks, cnn_weight=None, cnn_bias=None):
+        super().__init__()
+        self.ks = ks
+        self.ch_in = ch_in
+        self.ch_out = ch_out
+        self.img_dim = img_dim
+        self.num_pixels = img_dim**2
+        self.cnn_out_dim = cnn_dim_out(img_dim, ks, 1, 0)
+        
+        if cnn_weight is None:
+            cnn_weight = torch.empty(ch_out, ch_in, 4, ks, ks)
+            nn.init.kaiming_uniform_(cnn_weight, a=math.sqrt(5))
+        if cnn_bias is None:
+            cnn_bias = torch.empty(ch_out)
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(cnn_weight)
+            bound = 1 / math.sqrt(fan_in)
+            nn.init.uniform_(cnn_bias, -bound, bound)
+
+        self.conv_w = nn.ParameterList([
+            nn.Parameter(cnn_weight[i], requires_grad=True)
+            for i in range(ch_out)
+        ])
+        self.conv_b = nn.Parameter(cnn_bias, requires_grad=True)
+
+        self.mtx1, self.mtx2, self.weights, self.cparams_transform = load_mlp_weights(ckpt_path)
+        self.blocks = self.weights.shape[0]
+
     
+    def forward(self, img):
+        # Input (bs, ch_in, in_stab, img_size, img_size)
+        img = img.flatten(start_dim=3) # (bs, ch_in, in_stab, img_size ** 2)
+
+        # For non-matrix representation: 
+        result = []
+        for i in range(self.blocks):
+            result.append(img @ self.weights[i].T) # weights.T: img_size ** 2, bout_dim
+
+        result = torch.stack(result).to("cuda") # (blocks, bs, ch_in, in_stab, bout_dim)
+
+        out = []
+        per_result = result.permute(2, 3, 1, 4, 0) # (ch_in, stab_in, bs, bout_dim, blocks)
+        for i in range(self.ch_out):
+            out_temp = []
+            for j in range(self.ch_in):
+                mlp_out = []
+                conv_ij = self.conv_w[i][j] # ch_in, ks, ks
+                cp = []
+                for ii in range(16): 
+                    cp.append(torch.sum(torch.einsum("bij,bj->bi", self.cparams_transform[ii], conv_ij.view(4, self.ks ** 2)), axis=0))
+                cp = torch.stack(cp).view(4, 4, self.ks ** 2)
+                k_out = []
+                for k in range(4):
+                    m_out = []
+                    for m in range(4):
+                        mtx1_temp = (self.mtx1 * cp[k][m].view(self.ks ** 2, 1)).flatten() # ks ** 2 * merge_dim
+                        mtx2_temp = mtx1_temp @ self.mtx2 # (blocks,)
+                        result_temp = torch.sum(mtx2_temp * per_result[j][m], axis=2) + self.conv_b[i]
+                        m_out.append(result_temp)
+                    k_out.append(sum(m_out))
+                mlp_out = torch.stack(k_out).transpose(0, 1) # bs, 4, out
+                out_temp.append(mlp_out)
+            out.append(sum(out_temp))
+
+        out = torch.stack(out)
+
+        return out.transpose(0, 1).reshape(-1, self.ch_out, 4, self.cnn_out_dim, self.cnn_out_dim)
--- benchmark/MNISTClassifier.py
+++ benchmark/MNISTClassifier.py
@@ -9,7 +9,13 @@ from torch.autograd import Variable
 from groupy.gconv.pytorch_gconv.splitgconv2d import P4ConvZ2, P4ConvP4
 from groupy.gconv.pytorch_gconv.pooling import plane_group_spatial_max_pooling
 import torchsummary
+from MLP_gconv import MLPGConv_P4, MLPGConv_Z2
 
+project_path = '/project/a/aspuru/chengl43/rot_equiv_exp/benchmark/'
+ckpt_28 = project_path + 'checkpoints/9_28_Z2_checkpoint.ckpt'
+ckpt_26 = project_path + 'checkpoints/9_26_P4_checkpoint.ckpt'
+ckpt_24 = project_path + 'checkpoints/9_24_P4_checkpoint.ckpt'
+ckpt_10 = project_path + 'checkpoints/9_10_P4_checkpoint.ckpt'
 
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
@@ -41,14 +47,14 @@ kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 train_loader = torch.utils.data.DataLoader(
     datasets.MNIST('../data', train=True, download=True,
                    transform=transforms.Compose([
-                    #    transforms.RandomRotation(90),
+                       transforms.RandomRotation(90),
                        transforms.ToTensor(),
                        transforms.Normalize((0.1307,), (0.3081,))
                    ])),
     batch_size=args.batch_size, shuffle=True, **kwargs)
 test_loader = torch.utils.data.DataLoader(
     datasets.MNIST('../data', train=False, transform=transforms.Compose([
-                       transforms.RandomRotation(90),
+                       transforms.RandomRotation(180),
                        transforms.ToTensor(),
                        transforms.Normalize((0.1307,), (0.3081,))
                    ])),
@@ -59,33 +65,83 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = P4ConvZ2(1, 10, kernel_size=3)
-        self.conv2 = P4ConvP4(10, 10, kernel_size=3)
-        self.conv3 = P4ConvP4(10, 20, kernel_size=3)
+        self.conv2 = P4ConvP4(10, 20, kernel_size=3)
+        self.conv3 = P4ConvP4(20, 20, kernel_size=3)
         self.conv4 = P4ConvP4(20, 20, kernel_size=3)
-        self.fc1 = nn.Linear(4*4*20*4, 50)
+        self.fc1 = nn.Linear(4*20*4, 50)
         self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))  # 
+        x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = plane_group_spatial_max_pooling(x, 2, 2)
         x = F.relu(self.conv3(x))
         x = F.relu(self.conv4(x))
         x = plane_group_spatial_max_pooling(x, 2, 2)
+        x = rotate_and_pool_stablizer(x)
+        x = x.view(x.size()[0], -1)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
+    
+def rotate_and_pool_stablizer(x):
+    # input x: bs, ch, stab, img_size, img_size
+    rotated_x = torch.empty_like(x)
+    rotated_x[:, :, 0] = x[:, :, 0]
+    rotated_x[:, :, 1] = torch.rot90(x[:, :, 1], k=1, dims=(2, 3))
+    rotated_x[:, :, 2] = torch.rot90(x[:, :, 2], k=2, dims=(2, 3))
+    rotated_x[:, :, 3] = torch.rot90(x[:, :, 3], k=3, dims=(2, 3))
+    return torch.max(rotated_x, axis=2)[0]
+
+class CNNBaseline(nn.Module):
+    def __init__(self):
+        super(CNNBaseline, self).__init__()
+        self.conv1 = nn.Conv2d(1, 20, kernel_size=3)
+        self.conv2 = nn.Conv2d(20, 10, kernel_size=3)
+        self.conv3 = nn.Conv2d(10, 20, kernel_size=3)
+        self.conv4 = nn.Conv2d(20, 20, kernel_size=3)
+        self.fc1 = nn.Linear(4*20*4, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))  
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = F.max_pool2d(x, 2, 2)
         x = x.view(x.size()[0], -1)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x)
 
-model = Net()
-if args.cuda:
-    model.cuda()
+class MLPGConvNet(nn.Module):
+    def __init__(self):
+        super(MLPGConvNet, self).__init__()
+        self.conv1 = MLPGConv_Z2(ckpt_28, 28, 1, 20, ks=3)
+        self.conv2 = MLPGConv_P4(ckpt_26, 26, 20, 10, ks=3)
+        self.conv3 = MLPGConv_P4(ckpt_24, 24, 10, 20, ks=3)
+        self.conv4 = MLPGConv_P4(ckpt_10, 10, 20, 20, ks=3)
+        self.fc1 = nn.Linear(4*20*4, 50)
+        self.fc2 = nn.Linear(50, 10)
 
-optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
-torchsummary.summary(model, input_size=(1, 28, 28))
+    def forward(self, x):
+        x = F.relu(self.conv1(x))  
+        x = F.relu(self.conv2(x))
+        x = plane_group_spatial_max_pooling(x, 2, 2)
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = plane_group_spatial_max_pooling(x, 2, 2)
+        x = rotate_and_pool_stablizer(x)
+        x = x.view(x.size()[0], -1)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
 
-def train(epoch):
+def train(model, epoch):
     
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
@@ -103,7 +159,7 @@ def train(epoch):
                 100. * batch_idx / len(train_loader), loss.data))
 
 
-def test():
+def test(model):
     model.eval()
     test_loss = 0
     correct = 0
@@ -122,7 +178,25 @@ def test():
         100. * correct / len(test_loader.dataset)))
 
 
-for epoch in range(1, args.epochs + 1):
-    train(epoch)
-    test()
+if __name__ == '__main__':
+    model_type = 'MLP_GCNN'
+
+    if model_type == 'GCNN':
+        model = Net()
+    elif model_type == 'CNN':
+        model = CNNBaseline()
+    elif model_type == 'MLP_GCNN':
+        model = MLPGConvNet()
+    else:
+        raise NotImplementedError
     
+    if args.cuda:
+        model.cuda()
+
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    # torchsummary.summary(model, input_size=(1, 1, 28, 28))
+
+    for epoch in range(1, args.epochs + 1):
+        train(model, epoch)
+        test(model)
+            
